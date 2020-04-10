1869b3dfefdd39e63e2de9cd35b6edfa3a9d1d78
varunpandey0502 <varunpandey0502@gmail.com>
2020-04-10 19:52:28 +0530

    First commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# Flutter Dice
+
+A dice app built using Flutter
+
+## Getting Started
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes. See deployment for notes on how to deploy the project on a live system.
+
+### Prerequisites
+
+A few resources to get you started if this is your first Flutter project:
+
+- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
+- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
+
+For help getting started with Flutter, view our
+[online documentation](https://flutter.dev/docs), which offers tutorials,
+samples, guidance on mobile development, and a full API reference.
+
+### Installing
+
+1. Clone the Repository
+
+```
+git clone https://github.com/varunpandey0502/flutter-dice.git
+```
+
+2. Build and run
+
+```
+flutter run
+```
---
+++ lib/main.dart
@@ -0,0 +1,63 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+import 'package:share/share.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int diceNumber = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Dice'),
+        backgroundColor: Colors.blue,
+        actions: <Widget>[Icon(Icons.ac_unit), Icon(Icons.share)],
+      ),
+      body: Center(
+          child: Column(
+        children: <Widget>[
+          SizedBox(
+            height: 100,
+          ),
+          Image.asset(
+            'images/dice-$diceNumber.png',
+            height: 150,
+          ),
+          SizedBox(height: 25),
+          RaisedButton(
+              onPressed: () {
+                setState(() {
+                  diceNumber = Random().nextInt(6) + 1;
+                  // Share.share(text)
+                });
+              },
+              child: Text('Roll Dice'))
+        ],
+      )),
+    );
+  }
+}
